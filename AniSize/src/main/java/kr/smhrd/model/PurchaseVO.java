2df875617766493efe48a58b3a73339e5492f722
smhrd <smhrd@220.80.88.17>
2022-04-19 21:25:01 +0900

    -대현 중간푸쉬

---
+++ AniSize/src/main/java/kr/smhrd/model/PurchaseVO.java
@@ -0,0 +1,28 @@
+package kr.smhrd.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PurchaseVO {
+	private int ph_num;
+	private int mem_num;
+	private int pd_num;
+	private int ph_quantity;
+	private int stk_price;
+	private String stk_option;
+	private String stk_size;
+	private String ph_date;
+	private String recipient_address;
+	private String recipient;
+	private String recipient_tel;
+	private String delivery_requests;
+	private String payment;
+	private String pd_name;
+	private int review_done;
+	private String brand;
+	
+}
---
+++ AniSize/src/main/java/kr/smhrd/model/TestVO.java
@@ -0,0 +1,14 @@
+package kr.smhrd.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestVO {
+	private List<CartVO> buyList;
+}
